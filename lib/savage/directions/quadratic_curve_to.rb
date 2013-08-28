3bde4ad34e766e476340a8e9758b9d8f0f5e799f
hua.qiuh <hua.qiuh@alibaba-inc.com>
2013-08-28 14:38:45 +0800

    bugfix when quadratic curve path has empty control dot

--- lib/savage/directions/quadratic_curve_to.rb
+++ lib/savage/directions/quadratic_curve_to.rb
@@ -37,7 +37,7 @@ module Savage
         # relative line_to dont't need to be tranlated
         tx = ty = 0 if relative?
         transform_dot( target,  scale_x, skew_x, skew_y, scale_y, tx, ty)
-        transform_dot( control, scale_x, skew_x, skew_y, scale_y, tx, ty)
+        transform_dot( control, scale_x, skew_x, skew_y, scale_y, tx, ty) if control
       end
     end
   end
